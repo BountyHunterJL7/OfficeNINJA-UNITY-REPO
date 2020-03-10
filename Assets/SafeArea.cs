ee816dda8cd95cf30e5aee0c602edfbaf4d2715f
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2020-03-10 17:59:45 -0400

    Adding Safe area
    Adding a safe area where the boss will stop chasing

---
+++ Assets/SafeArea.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+    public bool safe;
+    void Start()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other) // When the player enters a "Safe" area
+    {
+        Debug.Log("Player is in the safe area");
+        safe = true;
+    }
+
+    public void OnTriggerStay(Collider other) // When the player stays in a "Safe" area
+    {
+        Debug.Log("Player is in the safe area");
+        safe = true;
+    }
+
+    public void OnTriggerExit(Collider other) // When the player exits a "Safe" area
+    {
+        Debug.Log("Player has left the safe area");
+        safe = false;
+    }
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
