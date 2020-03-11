2ca5b89e7253b5e4fe4683f70dcf3c01cbc8febd
Jacob Lapish <MightyMelonJL@gmail.com>
2020-03-10 23:41:07 -0400

    patrol and chase
    the boss now patrols the office and if you get too close (7 flaot units at the moment, not like a line of sight) then he chases you until you escape him

---
+++ Assets/BossAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+    Animator anim;
+    public GameObject player;
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("distance", Vector3.Distance(transform.position,player.transform.position));
+    }
+}
