f7483e14b5dd8ae08592f6451754e49bd2eb541f
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2020-03-12 22:23:13 -0400

    Revert "gamer time :100:"
    This reverts commit 02d9f106800c95c30d6020045e140ea940a1b0c2.

--- Assets/ObjectScript.cs
+++ Assets/ObjectScript.cs
@@ -27,10 +27,10 @@ public class ObjectScript : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        Ray cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);  
+        Ray cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         //todo, add raycast check for objects
         HoldCheck = PlayerPosition.GetComponent<HoldCheck>().IsHolding;
-        if (Physics.SphereCast(cursorRay, 0.7f, out hit, 0) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        if (Physics.Raycast(cursorRay, out hit) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             Debug.LogWarning(hit.collider.name);
             if (!HoldCheck && Input.GetMouseButtonDown(0) && PlayerDistance() < 3f && !Thrown && hit.collider.name == gameObject.name)
