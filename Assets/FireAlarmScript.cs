11f925df066af3af165182017e68381c92836952
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2020-03-27 13:31:09 -0400

    Added NPC, Firealarm(still testing)

---
+++ Assets/FireAlarmScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAlarmScript : MonoBehaviour
+{
+    public Transform player;
+    public float distance;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            if (other.gameObject.tag.Equals("Player") == true)
+            {
+                Debug.Log("OHH shitt");
+            }
+        
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(cursorRay, out hit, 100.0f))
+        {
+            distance = Vector3.Distance(player.position, transform.position);
+            if (hit.transform.name == "mesh_firealarm" && Input.GetMouseButtonDown(0) && distance<2.0)
+            {
+                Debug.Log("You clicked");
+                Debug.Log(distance);
+
+            }
+        }
+
+
+
+
+    }
+
+}
---
+++ Assets/NPCBehaviourScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehaviourScript : MonoBehaviour
+{
+    Animator NPCAnim;
+    public GameObject player;
+    private float countdown = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCAnim = GetComponent<Animator>();
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        if (collision.gameObject.tag.Equals("Player") == true)
+        {
+            NPCAnim.SetBool("IsNear", true);
+            Debug.Log("something entered");
+
+            
+        }
+        
+    }
+
+    
+    private void OnTriggerStay(Collider collision)
+    {
+        
+        if (collision.gameObject.tag.Equals("Player") == true)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                NPCAnim.SetBool("IsNear", false);
+                countdown = 10;
+                
+            }
+            Debug.Log(countdown);
+        }
+
+    }
+    
+    private void OnTriggerExit(Collider collision)
+    {
+        
+        if (collision.gameObject.tag.Equals("Player") == true)
+        {
+            NPCAnim.SetBool("IsNear", false);
+            Debug.Log("something left");
+            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
