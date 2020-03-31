0ce6a6442ad5a8bf7dbba8d6d748d964b00356f7
Ethan McIntyre <48802061+ethanmci@users.noreply.github.com>
2020-03-30 23:43:18 -0400

    FINAL BETA PUSH :smile:

--- Assets/FindThrown.cs
+++ Assets/FindThrown.cs
@@ -30,7 +30,7 @@ public class FindThrown : NPCbaseFSM
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
 
-        if (Distractions.Length == 0) return;
+        if (Distractions.Length == 0 || Distractions[0] != null) return;
         agent.destination = Distractions[0].transform.position;
         /*
         if (Vector3.Distance(Distractions[0].transform.position,
