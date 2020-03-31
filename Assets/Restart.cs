cbc573185e60aded7be47d419a648b3dfadb0eff
Jacob Lapish <MightyMelonJL@gmail.com>
2020-03-30 23:27:50 -0400

    game now

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -8,6 +8,6 @@ public class Restart : MonoBehaviour
     // Start is called before the first frame update
     public void restartGame()
     {
-        SceneManager.LoadScene("TESTING VER");
+        SceneManager.LoadScene("GAME");
     }
 }
